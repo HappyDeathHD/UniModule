7370053233857296f521f9b1ed127e5f1d999db9
Bulat Utyakaev <utiakaev@bpcbt.com>
2020-02-25 15:45:08 +0300

    json regexp fix

--- UniModule/deskapp/src/main/java/ru/bpcbt/logger/ReportPanel.java
+++ UniModule/deskapp/src/main/java/ru/bpcbt/logger/ReportPanel.java
@@ -8,8 +8,8 @@ public class ReportPanel extends JPanel {
 
     public ReportPanel() {
         setLayout(new BorderLayout());
-        JScrollPane scroll = new JScrollPane(ReportPane.getReportPane());
-        DefaultCaret caret = (DefaultCaret)ReportPane.getReportPane().getCaret();
+        final JScrollPane scroll = new JScrollPane(ReportPane.getReportPane());
+        final DefaultCaret caret = (DefaultCaret) ReportPane.getReportPane().getCaret();
         caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
         scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         add(scroll, BorderLayout.CENTER);
